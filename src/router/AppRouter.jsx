71a760a5db312e169ba0f4eaa7f6c3e60a5093d0
ucangun <149247682+ucangun@users.noreply.github.com>
2024-08-22 12:56:18 +0200

    "Remove isAuthenticated state and prop from AuthProvider, update PrivateRouter import"

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,15 +5,18 @@ import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 import Movies from "../pages/Movies";
 import MovieDetails from "../pages/MovieDetails";
+import PrivateRouter from "./PrivateRouter";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/" element=<Home /> />
-      <Route path="/login" element=<Login /> />
-      <Route path="/signup" element=<SignUp /> />
-      <Route path="/movies" element=<Movies /> />
-      <Route path="/details/:id" element=<MovieDetails /> />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/details/:id" element={<PrivateRouter />}>
+        <Route path="" element={<MovieDetails />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/router/PrivateRouter.jsx
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
+
+const PrivateRouter = () => {
+  const { user } = useContext(AuthContext);
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRouter;
